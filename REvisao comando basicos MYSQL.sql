6ce3b23089bb7915c2367fde32ad33514768038c
FelipeXavier99 <felipe.cvxavier@gmail.com>
2023-03-30 09:06:55 -0300

    commit inicial

---
+++ REvisao comando basicos MYSQL.sql
@@ -0,0 +1,26 @@
+--REVISAO MYQL WORBENC
+
+
+    select *from teste_mysql
+
+
+
+
+
+alter table teste rename to teste_mysql
+  alter table teste_mysql change nasciment nascimento  int
+ 
+
+
+insert into teste_mysql (id,idade,nascimento) values(3,20,"2010-10-10")
+
+SELECT YEAR(nascimento)  FROM teste_mysql   -- da pra botar (month,day)  
+-- alter table aa rename  TO  teste_mysql
+
+-- delete from aa where id>=0
+ -- drop table teste_mysql
+create table teste_mysql(
+id int not null auto_increment  primary key,
+idade int,
+nascimento date 
+)
